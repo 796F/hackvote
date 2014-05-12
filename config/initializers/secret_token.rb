4d985b08f0965a918f823750596cab219d7f796f
Michael Xia <xia.umd@gmail.com>
2014-05-12 16:43:26 -0700

    empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackvote::Application.config.secret_token = '1e9ce630807a952fb6d20c607873cf61b56892e3d00dad55be7e89f6877944ba299886238d634ed410b9d49ccaddf703dd95621d1ab6c5c0191972b647b01802'
