bcf4f11a5457bae1f5866b023c37790bcc1210d4
Michael Xia <xia.umd@gmail.com>
2014-05-16 14:14:09 -0700

    added validation for null title

--- db/migrate/20140514020428_create_hackdays.rb
+++ db/migrate/20140514020428_create_hackdays.rb
@@ -1,7 +1,7 @@
 class CreateHackdays < ActiveRecord::Migration
   def change
     create_table :hackdays do |t|
-      t.string :title
+      t.string :title, :null => false
       t.date :day
       t.string :img_url
 
