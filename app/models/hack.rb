ab7105cd7c6890e4fc435ab7f85c19574555e0e1
Michael Xia <xia.umd@gmail.com>
2014-05-19 15:45:24 -0700

    require desc now that url is not required

--- app/models/hack.rb
+++ app/models/hack.rb
@@ -2,7 +2,7 @@ class Hack < ActiveRecord::Base
   belongs_to :hackday
   attr_accessible :hackday_id, :hack_url, :img_url, :title, :votes, :owner, :description 
 
-  validates :title, :hackday, :presence => true
+  validates :title, :hackday, :description, :presence => true
   validates :title, :length => { :minimum => 3 }
-  
+
 end
